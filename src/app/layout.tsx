0466046aea23fad1db136a2cafa1257677ddcef5
Nomada 09 <nomada09@Nomadas-Mac-mini.local>
2026-02-21 23:44:40 -0400

    New favicon: white W on pine green circle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,8 @@ export default function RootLayout({
       className={`${instrumentSerif.variable} ${GeistSans.variable} ${GeistMono.variable}`}
     >
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
